0507a9338e4c41f8c827dd7c0a7e56f83c2b52fe
kevin <kevin.hijlkema@meteoconsult.com>
2019-05-23 12:21:01 +0200

    Use the pod in a ViewController container app

--- Example/LinkageError/ViewController.swift
+++ Example/LinkageError/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import LinkageError
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let obj = LinkageErrorAPI()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ LinkageError/Classes/LinkageErrorAPI.swift
@@ -0,0 +1,17 @@
+//
+//  LinkageErrorAPI.swift
+//  Pods
+//
+//  Created by Kevin Hijlkema on 23/05/2019.
+//
+
+import Foundation
+import BrightcovePlayerSDK
+import BrightcoveIMA
+import GoogleInteractiveMediaAds
+
+public class LinkageErrorAPI: NSObject {
+    var adsManager: IMAAdsManager!
+    var kBCOVIMALifecycleEventAdsLoaderLoaded: IMAAdsManager!
+    var playerView: BCOVPUIPlayerView!
+}
